7929181d3a362f91145fa219b321830306a47d99
sel-erdem <sel1905erdem@gmail.com>
2021-06-30 21:21:23 -0400

    pretending to do useful stuff

--- src/test/java/HelloGroup17.java
+++ src/test/java/HelloGroup17.java
@@ -1,5 +1,17 @@
 public class HelloGroup17 {
+
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello Group 17! from Masha");
+
+
+        System.out.println("This is me pretending to do something :)");
+
+
+
+
+
     }
 }
