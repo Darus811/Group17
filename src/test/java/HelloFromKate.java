699aadc08bd36888d2beadbf44491c04a93192a5
KateCherkasova <cath.cherkasova@gmail.com>
2021-06-30 21:29:19 -0400

    added one more line

--- src/test/java/HelloFromKate.java
+++ src/test/java/HelloFromKate.java
@@ -1,5 +1,7 @@
 public class HelloFromKate {
     public static void main(String[] args) {
         System.out.println("Hello...it's me :,-) ");
+        //adding one more line of code as requested
+        System.out.println("final line of code");
     }
 }
